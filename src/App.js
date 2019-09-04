9764aec78703ed5689f267b9dfc1a3bb445858f8
ae <ae@Adam-dv9700-Notebook-PC>
2019-09-04 13:01:28 -0400

    adds Auth, Profile pages & routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './containers/Home/Home';
+import Auth from './containers/Auth/Auth';
 import About from './components/About';
+import Profile from './components/Profile';
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <Switch>
+          <Route path="/auth" component={Auth} />
+          <Route path="/profile" component={Profile} />
           <Route path="/about" component={About} />
           <Route path="/" exact component={Home} />
         </Switch>
       </header>
     </div>
   );
-}
+};
 
 export default App;
